487a4f5cd3932814d91814f85c14021fc2b97a6e
Kyuuri <dark_e2102@hotmail.com>
2013-11-20 16:35:50 +0700

    add ledger ui, change imageButton in inventory to simple button.

--- src/com/bbaf/mpos/saleledger/ui/LedgerUIActivity.java
+++ src/com/bbaf/mpos/saleledger/ui/LedgerUIActivity.java
@@ -7,13 +7,39 @@ import com.bbaf.mpos.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class LedgerUIActivity extends Activity {
+	
+	private TabHost tabLedger;
+	private TabSpec tabByDate;
+	private TabSpec tabByWeek;
+	private TabSpec tabByMonth;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ledger_ui);
+		
+		tabLedger = (TabHost) findViewById(R.id.tabLedger);
+		tabLedger.setup();
+		
+		tabByDate = tabLedger.newTabSpec("tabdate");
+		tabByDate.setContent(R.id.tabdate);
+		tabByDate.setIndicator("By Date");
+		tabLedger.addTab(tabByDate);
+		
+
+		tabByWeek = tabLedger.newTabSpec("tabweek");
+		tabByWeek.setContent(R.id.tabweek);
+		tabByWeek.setIndicator("By Week");
+		tabLedger.addTab(tabByWeek);
+		
+		tabByMonth = tabLedger.newTabSpec("tabmonth");
+		tabByMonth.setContent(R.id.tabmonth);
+		tabByMonth.setIndicator("By Month");
+		tabLedger.addTab(tabByMonth);
 	}
 
 	@Override
